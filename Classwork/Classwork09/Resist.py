a4bcacf36b9960c69b9087b5bed6ed62e00e451d
amindell11 <amindell11@gmail.com>
2024-03-14 14:24:05 -0700

    updated resist to offer parallel and series modes

--- Classwork/Classwork09/Resist.py
+++ Classwork/Classwork09/Resist.py
@@ -5,19 +5,28 @@ from MetricPrefix import prefix_to_value
 def main():
     try:
         # Convert command line arguments (except the script name) to integers
-        admitances = list(map(calcAdmittance, sys.argv[1:]))  # Skip the script name and convert
-        print(1/sum(admitances))
+        mode = sys.argv[1]
+        if(mode == 'p'):
+            admitances = list(map(calcAdmittance, sys.argv[2:]))  # Skip the script name and convert
+            print(1/sum(admitances))
+        elif(mode == 's'):
+            resistances = list(map(calcResist, sys.argv[2:]))  # Skip the script name and convert
+            print(sum(resistances))
+        else:
+            raise ValueError(f"improper mode: "+sys.argv[1])
     except ValueError as e:
-        print("Error: Please ensure all arguments are integers.")
+        print("Error: Please ensure all arguments are correct.")
 def calcAdmittance(r):
-    admit = 0
+    return 1/calcResist(r)
+def calcResist(r):
+    resist = 0
     n=1
     for character in r:
         if(not character.isdigit()):
             r = r.replace(character,'')
             n = prefix_to_value(character)
             break
-    admit = 1/(n*float(r))
-    return admit    
+    resist = n*float(r)
+    return resist      
 
 main()
